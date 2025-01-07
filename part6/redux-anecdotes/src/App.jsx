7cf2b7e08e4bedf0b7a4e1a541e2db810a2695a4
Omar Ehab <89408282+omarehab24@users.noreply.github.com>
2025-01-07 09:03:02 +0200

    Add part 6 - exercises 6.3 - 6.8

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,16 @@
+import Anecdotes from "./components/Anecdotes"
+import NewAnecdote from "./components/NewAnecdote"
+
+const App = () => {
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+     <Anecdotes />
+     <NewAnecdote />
+     
+    </div>
+  )
+}
+
+export default App
